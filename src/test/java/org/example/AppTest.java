5d7988ea85db497993c95f402b940cfa133d792d
Smita Bose <smitabose7826@gmail.com>
2026-04-01 23:16:54 +0530

    Merge branch 'master' of https://github.com/smita-bose/Learning

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,38 +1,25 @@
 package org.example;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.testng.Assert.assertEquals;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+@Test
+public class AppTest {
+    @Test
+    public void testGoogleTitle(){
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        try {
+            driver.get("https://www.google.com");
+            String expectedTitle = "Google";
+            String actualTitle = driver.getTitle();
+            assertEquals(actualTitle, expectedTitle);
+            System.out.println("Test passed: Title is " + actualTitle);
+        } finally {
+            driver.quit();
+        }
     }
 }
